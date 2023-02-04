ddd1e37e6b04731c72c3b4071b5f17c64a7e15bc
Kweku Adarkwa <1kweku@Kwekus-MacBook-Air-2.local>
2023-02-03 23:13:11 -0500

    navTabs functional

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -22,16 +22,28 @@ export default function NavTabs(props) {
   return (
     <nav>
       <ul className="nav" style={styles.nav}>
-        <ol className="nav-item" onClick={props.handlePageChange}>
+        <ol
+          className="nav-item"
+          onClick={(e) => props.handlePageChange("About")}
+        >
           <a href="#about"> About me </a>{" "}
         </ol>
-        <ol className="nav-item" onClick={props.handlePageChange}>
+        <ol
+          className="nav-item"
+          onClick={(e) => props.handlePageChange("Contact")}
+        >
           <a href="#contact"> Contact </a>{" "}
         </ol>
-        <ol className="nav-item" onClick={props.handlePageChange}>
+        <ol
+          className="nav-item"
+          onClick={(e) => props.handlePageChange("Resume")}
+        >
           <a href="#resume"> Resume </a>{" "}
         </ol>
-        <ol className="nav-item" onClick={props.handlePageChange}>
+        <ol
+          className="nav-item"
+          onClick={(e) => props.handlePageChange("Portfolio")}
+        >
           <a href="#portfolio"> Portfolio </a>{" "}
         </ol>
       </ul>
